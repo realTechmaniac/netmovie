19ada4731bf179f8cd9b5d9a14c0be5de2ad8bd0
realTechmaniac <ayomide.adebayo19@gmail.com>
2021-11-14 23:17:36 +0100

    new updates added

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,6 +7,7 @@
     <title>Movie App</title>
     <link rel ="stylesheet" href = "{{asset('css/app.css')}}">
     @livewireStyles
+    <script defer src="https://unpkg.com/alpinejs@3.5.0/dist/cdn.min.js"></script>
 </head>
 <body class = "font-sans bg-gray-900 text-white">
     <nav class ="border-b border-gray-800">
